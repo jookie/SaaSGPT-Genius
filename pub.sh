30b08a7587f7fc6315d9732826f4698260197d92
jookie <dovpeles@gmail.com>
2024-02-29 16:42:30 -0500

    Initial commit

--- pub.sh
+++ pub.sh
@@ -21,21 +21,4 @@ git push
 
 #
 
-## git remote add origin <remote_repository_URL>
-## 👇️ install yarn cli globally
-# npm install -g yarn
-# heroku git:clone -a ches1 herochess1
-# Connected to  jookie/gaj4 by jookie jookie
-# heroku git:clone -a gaj4 hero_gaj4
-# Connected to  jookie/info_soft by jookie jookie
-# heroku git:clone -a info22 hero_info22
-# heroku git:clone -a live02 hero_live02
-# heroku git:clone -a oxy2 hero_oxy
-# http://localhost:9001/demos/video-conferencing.html
-# Unable to listen on port: 9001 
-# http://localhost:9001/ is already in use. Please kill below processes using "kill PID". 
-# ------------------------------
-# COMMAND  PID     USER   FD   TYPE             DEVICE SIZE/OFF NODE NAME
-# node    5926 MacAdmin   23u  IPv4 0x9c6759e5df48fca9      0t0  TCP *:e
-
 
